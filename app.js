3c7504531312948c7372a1b9a0fa38bb0bf2de3c
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-30 11:45:01 +0100

    main funcinality done

---
+++ app.js
@@ -0,0 +1,55 @@
+
+
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    const values = document.querySelectorAll("#read, #author, #pages, #title");
+    const book = new Book();
+    values.forEach(value => {
+        if(value.id != "read"){
+            book[value.id] = value.value;
+        }else if(value.checked){
+            book[value.id] = "<input type='checkbox' id='read' name='read' value='true' checked>";
+        }else{
+            book[value.id] = "<input type='checkbox' id='read' name='read' value='true'>";
+        }
+ 
+    });
+    myLibrary.push(book);
+    console.log(book.read);
+    refresh();
+}
+let myLibrary = [];
+const refresh = () =>{
+    const table = document.querySelector("table");
+    table.innerHTML = `
+    <tr>
+        <th>title</th>
+        <th>pages</th>
+        <th>author</th>
+        <th>read</th>
+        <th>remove</th>
+    </tr>`
+    myLibrary.forEach(book => {
+        const row = document.createElement("tr");
+        for (const key in book) {
+            const cell = document.createElement("td");
+            cell.innerHTML = book[key];
+            row.appendChild(cell);
+        }
+        const cell = document.createElement("td");
+        cell.innerHTML = `<button class="remove" onclick="removeBook(${myLibrary.indexOf(book)})">remove</button>`;
+        row.appendChild(cell);
+        table.appendChild(row);
+        console.log(book);
+    });
+}
+const removeBook = (index) =>{
+    myLibrary.splice(index, 1);
+    refresh();
+}
